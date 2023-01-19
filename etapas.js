fa5bb463c765696c4607f2702155f9ddc5bf3fb9
Wesleydf <wesleydefaria@hotmail.com>
2023-01-19 01:40:20 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,76 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR(A)',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'38111.jpg', legenda: 'Vereador', small:true}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrana da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'77222.jpg', legenda: 'Vereadora', small:true}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:'99_1.jpg', legenda: 'Prefeito'},
+                    {url:'99_2.jpg', legenda: 'Vice-Prefeita', small:true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'DEF',
+                vice: 'Zul',
+                fotos:[
+                    {url:'84.jpg', legenda: 'Prefeito'},
+                    {url:'84_2.jpg', legenda: 'Vice-Prefeito', small:true}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '22',
+                nome: 'Bolsonaro',
+                partido: 'PL',
+                vice: 'Braga Netto',
+                fotos:[
+                    {url:'22.jpg', legenda: 'Presidente'},
+                    {url:'22_1.jpg', legenda: 'Vice-Presidente', small:true}
+                ]
+            },
+            {
+                numero: '13',
+                nome: 'Lula',
+                partido: 'PT',
+                vice: 'Geraldo Alckmin',
+                fotos:[
+                    {url:'13.jpg', legenda: 'Presidente'},
+                    {url:'13_1.jpg', legenda: 'Vice-Presidente'},
+                ]
+            },
+        ]
+    },
+];
